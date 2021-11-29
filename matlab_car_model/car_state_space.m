8ca621b14d5632c51ad8d789484ccbb7a5dd6bd1
David O. Rico <david.rico@datavision.cz>
2021-11-29 10:50:58 +0100

    MPC control N3

--- matlab_car_model/car_state_space.m
+++ matlab_car_model/car_state_space.m
@@ -25,3 +25,35 @@ A = [-(2*Cf+2*Cr)/m/Vx, 0, -Vx-(2*Cf*lf-2*Cr*lr)/m/Vx, 0;
 B = [2*Cf/m 0 2*Cf*lf/Iz 0]';
 C = [0 0 0 1; 0 1 0 0];
 vehicle = ss(A,B,C,0);
+
+%horizon N=3
+x = [0; 0; 0; 0];
+Q = C'*C;
+Q_spec = blkdiag(Q, Q, Q);
+B_spec = [zeros(4, 3); B zeros(4, 2); A*B B zeros(4, 1)];
+R = 1/10;
+R_spec = blkdiag(R, R, R);
+A_spec = [eye(4); A; A^2];
+
+H = 2*(B_spec'*Q_spec*B_spec + R_spec);
+f = 2*x'*A_spec'*Q_spec*B_spec;
+A_constraints = [1 0 0; -1 0 0; 0 1 0; 0 -1 0; 0 0 1; 0 0 -1];
+b_constraints = ones(6, 1);
+
+%MPC controller
+res = zeros(4, 41);
+u_res = zeros(1, 41);
+for k = 1:41
+    f = 2*x'*A_spec'*Q_spec*B_spec;
+    min_of_J = quadprog(H,f,A_constraints,b_constraints);
+    u = min_of_J(1,1);
+    x = A*x + B*u;
+    res(:, k) = x;
+    u_res(:, k) = u;
+end
+
+figure(2);
+plot(res');
+
+figure(3);
+plot(u_res');
