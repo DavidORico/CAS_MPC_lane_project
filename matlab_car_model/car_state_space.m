f324f2f5d7a3f454257206262bb7d2e74fc46193
David O. Rico <david.rico@datavision.cz>
2021-11-29 09:21:01 +0100

    modified state space

--- matlab_car_model/car_state_space.m
+++ matlab_car_model/car_state_space.m
@@ -18,8 +18,10 @@ lr = 1.6;
 Cf = 19000;
 Cr = 33000;
 
-A_new = [-(2*Cf+2*Cr)/(m*Vx), 0, -Vx-(2*Cf*lf+2*Cr*lr)/(m*Vx);
-    0, 0, 1;
-    -(2*Cf*lf+2*Cr*lr)/(Iz*Vx), 0,-(2*Cf*lf*lf+2*Cr*lr*lr)/(Iz*Vx) ];
-B_new = [(2*Cf)/m; 0; (2*lf*Cf)/Iz];
-C_new = [0 1 0];
+A = [-(2*Cf+2*Cr)/m/Vx, 0, -Vx-(2*Cf*lf-2*Cr*lr)/m/Vx, 0;
+     0, 0, 1, 0;
+     -(2*Cf*lf-2*Cr*lr)/Iz/Vx, 0, -(2*Cf*lf^2+2*Cr*lr^2)/Iz/Vx, 0;
+     1, Vx, 0, 0];
+B = [2*Cf/m 0 2*Cf*lf/Iz 0]';
+C = [0 0 0 1; 0 1 0 0];
+vehicle = ss(A,B,C,0);
