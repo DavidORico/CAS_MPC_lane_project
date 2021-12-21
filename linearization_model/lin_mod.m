34e210b283d9bae215a92bcef88f959807984db9
Jef <Jefbillet@outlook.com>
2021-12-21 15:03:03 +0100

    edit

--- linearization_model/lin_mod.m
+++ linearization_model/lin_mod.m
@@ -1,14 +1,16 @@
 %recreating the Linearization paper
+clear all
 %basic parameters
 T = 0.1;
-N = 5;
+N = 30;
 u_max = [0.47;
         3.77];
 u_min = -u_max;
-Q = diag([1 1 0.5]);
-Q_spec = blkdiag(Q, Q, Q, Q, Q);
-R = diag([0.1 0.1]);
-R_spec = blkdiag(R, R, R, R, R);
+% Q = diag([1 1 0.5]);
+% Q_spec = blkdiag(Q, Q, Q, Q, Q);
+% R = diag([0.1 0.1]);
+% R_spec = blkdiag(R, R, R, R, R);
+
 %create reference trajectory
 x = [-1 -1 0]';
 num_reference_points=150;
@@ -32,62 +34,50 @@ plot(ref_x(1,:), ref_x(2, :))
 res_x = [];
 res_u = [];
 x = [-0.8 -0.7 0]';
-for k = 1:num_reference_points-5
+tic
+for k = 1:num_reference_points
    %% update the A and B matrix
-   A = [1 0 -ref_speed(k)*sin(ref_angle(k))*T;
-        0 1 ref_speed(k)*cos(ref_angle(k))*T;
-        0 0 1];
-   B = [cos(ref_angle(k))*T 0;
-        sin(ref_angle(k))*T 0;
-        0 T];
+   [A_spec,B_dash,Q_spec,R_spec,A,B] = statespace(ref_u,ref_x,k,N,T);
+%    A = [1 0 -ref_speed(k)*sin(ref_angle(k))*T;
+%         0 1 ref_speed(k)*cos(ref_angle(k))*T;
+%         0 0 1];
+%    B = [cos(ref_angle(k))*T 0;
+%         sin(ref_angle(k))*T 0;
+%         0 T];
    %% update the A and B matrices corresponding to the horizon 
-   %A_spec = [A^(k-1);
-   %           A^(k);
-   %           A^(k+1);
-   %           A^(k+2);
-   %           A^(k+3)];
-   A_spec = [eye(3);
-             A;
-             A^2;
-             A^3;
-             A^4];
-   
-   %b_row1 = [k-2, k-3, k-4, k-5, k-6];
-   %b_row2 = b_row1 + 1;
-   %b_row3 = b_row1 + 2;
-   %b_row4 = b_row1 + 3;
-   %b_row5 = b_row1 + 4;
-   %B_spec = [b_row1;
-   %          b_row2;
-   %          b_row3;
-   %          b_row4;
-   %          b_row5];
-   %B_dash = apply_power(A, B, B_spec);2];
-   zr = zeros(3,2);
-   B_dash = [zr, zr, zr, zr, zr;
-             B, zr, zr, zr, zr;
-             A*B, B, zr, zr, zr;
-             A^2*B, A*B, B, zr, zr;
-             A^3*B, A^2*B, A*B, B, zr];
+%    A_check = [eye(3);
+%              A;
+%              A^2;
+%              A^3;
+%              A^4];
+%    
+%    zr = zeros(3,2);
+%    B_check = [zr, zr, zr, zr, zr;
+%              B, zr, zr, zr, zr;
+%              A*B, B, zr, zr, zr;
+%              A^2*B, A*B, B, zr, zr;
+%              A^3*B, A^2*B, A*B, B, zr];
    %% calculate u_min using quadprog
-   H = 2*(B_dash'*Q_spec*B_dash + R_spec);
-   f = 2*B_dash'*Q_spec*A_spec*(x-ref_x(:, k));
+   H = double(2*(B_dash'*Q_spec*B_dash + R_spec));
+   f = double(2*B_dash'*Q_spec*A_spec*(x-ref_x(:, k)));
    constraints_l = zeros(N*2*2, N*2);
    constraints_l_p = [1 0; -1 0; 0 1; 0 -1];
-   constraints_l(1:4, 1:2) = constraints_l_p;
-   constraints_l(5:8, 3:4) = constraints_l_p;
-   constraints_l(9:12, 5:6) = constraints_l_p;
-   constraints_l(13:16, 7:8) = constraints_l_p;
-   constraints_l(17:20, 9:10) = constraints_l_p;
+   for c = 1:4:N*4-3
+       constraints_l(c:c+3, ((c-1)/4)*2+1:((c-1)/4)*2+2) = constraints_l_p;
+   end
    constraints_r = [0.47; 0.47; 3.77; 3.77];
-   constraints_r = [constraints_r; constraints_r; constraints_r; constraints_r; constraints_r];
-   u_quad = quadprog(H, f, constraints_l, constraints_r);
+   final_r = [];
+   for c =1:N
+       final_r = [final_r ; constraints_r];
+   end
+   u_quad = quadprog(H, f, constraints_l, final_r);
    u_min = u_quad(1:2, 1);
    %% go to next step
    x = A*x + B*u_min;
    res_x = [res_x, x];
    res_u = [res_u, u_min];
 end
+toc
 figure(2)
 plot(res_x(1,:), res_x(2, :));
 figure(3)
