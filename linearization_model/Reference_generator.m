8ec1db6fef3c090265589d5a6afa6b1b6a20012a
Jef <Jefbillet@outlook.com>
2021-12-30 17:37:13 +0100

    updated

--- linearization_model/Reference_generator.m
+++ linearization_model/Reference_generator.m
@@ -1,25 +1,28 @@
-function [ref_x,ref_u] = Reference_generator(global_xy, T)
+function [ref_x_outer,ref_u,curvature_radius] = Reference_generator(global_xy, T)
 %Reference_generator (theta,v, delta)
-L = 2;
-Lfw = 2;    %corresponds to distance from the trajectory
+L = 2.5;
+Lfw = 2;    %corresponds to looking distance from the trajectory
+WL = 4; %Width of a lane;
 theta = [];
-[a,b] = size(global_xy);
-ref_x = [];
+[x_inner, y_inner, x_outer, y_outer, curvature_radius] = parallel_curve(global_xy(1,:), global_xy(2,:), WL/2,0,0);
+[b,a] = size(x_inner);
+ref_x_inner = [x_inner';y_inner'];
+ref_x_outer = [x_outer';y_outer'];
 ref_u = zeros(a,b);
-%required: Local angles and steering angle/rate
+
 %% Local angles
 for k=1:b
     if k < b
-       theta = [theta, acos(dot(global_xy(:,k),global_xy(:,k+1))/(norm(global_xy(:,k))*norm(global_xy(:,k+1))))];
+       theta = [theta, acos(dot(ref_x_outer(:,k),ref_x_outer(:,k+1))/(norm(ref_x_outer(:,k))*norm(ref_x_outer(:,k+1))))];
     else
         theta = [theta,0];
     end
 end
-ref_x = real([global_xy;theta]);
+ref_x_outer = real([ref_x_outer;theta]);
 %% Steering angle
 for k = 1:b
     if k < b
-        delta = -atan((L*sin(ref_x(3,k+1)))/((Lfw/2)+Lfw*cos(ref_x(3,k+1))));
+        delta = -atan((L*sin(ref_x_outer(3,k+1)))/((Lfw/2)+Lfw*cos(ref_x_outer(3,k+1))));
         ref_u(2,k) = real(delta);
     else
         delta = 0;
@@ -31,7 +34,7 @@ Vr = 1;
 Kp = 0.005;
 for k = 1:b
     if k < b
-        distancetonext = norm(global_xy(:,k+1)-global_xy(:,k),2);
+        distancetonext = norm(ref_x_outer(:,k+1)-ref_x_outer(:,k),2);
     else
         distancetonext = 0;
     end
